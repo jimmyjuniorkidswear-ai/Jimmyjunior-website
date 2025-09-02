95bd225dc05cb84e88ca73930ccf2dd35bdcd77c
jimmyjuniorkidswear-ai <jimmyjuniorkidswear@gmail.com>
2025-09-02 18:29:28 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["images.unsplash.com"], // yaha apni product images ka domain add karna
-  },
-};
-
-module.exports = nextConfig;
+const nextConfig = {}
+module.exports = nextConfig
